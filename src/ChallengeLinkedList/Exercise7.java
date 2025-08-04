5a597feca2a4cf70580498484b79c1c6e322470e
Eusebiu Tatarciuc <evsebiu@gmail.com>
2025-08-04 18:28:04 +0300

    pushhhhhh

---
+++ src/ChallengeLinkedList/Exercise7.java
@@ -0,0 +1,9 @@
+package ChallengeLinkedList;
+
+//7. Write a Java program to insert the specified element at the front of a linked list.
+
+public class Exercise7 {
+    public static void main(String[] args) {
+
+    }
+}
