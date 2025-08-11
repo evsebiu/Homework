7ae74608442c9556f6c45566830ae9379920bb39
Eusebiu Tatarciuc <evsebiu@gmail.com>
2025-08-11 18:34:21 +0300

    pushhhhhh

---
+++ src/ChallengeEqualsHashcodes/Rectangle.java
@@ -0,0 +1,24 @@
+package ChallengeEqualsHashcodes;
+
+import java.util.Objects;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Rectangle other = (Rectangle) obj;
+        return width == other.width && height == other.height;
+    }
+}
